ace87b47a308c98ac46c9c9930111d8eb1fe59a5
Maurice Yang <mauriceyangmy28@gmail.com>
2022-01-17 16:41:42 +0700

    Making the site SEO Friendly

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,20 @@ import About from '../components/About'
 import Project from '../components/Project'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
+import Head from 'next/head'
 
 export default function Home(props) {
 
   return (
     <div className="App">
+      <Head>
+        <title>Maurice Yang</title>
+          <meta charset="UTF-8"/>
+          <meta name="description" content="Maurice Yang is a fullstack web developer who specializes in developing websites using React JS and Django, constantly finding a way to improve both my technical skills and teamwork with other people. I&apos;m passionate to learn about all the wonderful new technologies. Had experience in deploying websites both in Heroku and Netlify."/>
+          <meta name="keywords" content="Maurice Yang"/>
+          <meta name="author" content="Maurice Yang"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
         <div id="home">
           <Header/>
           <Hero/>
